669410af245528264305ddb8d6229d29710770a0
viniciusshenri96 <vinicius_henrique96@hotmail.com>
2022-07-27 12:04:03 -0300

    Selecionando elementos HTML com código JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 'use strict';
+document.querySelector('.message').textContent = 'Correct Number! 🎉';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;

14371305410c4f61c1a0dbcd0a6991326b845f59
viniciusshenri96 <vinicius_henrique96@hotmail.com>
2022-07-27 12:29:42 -0300

    Checkando se o valor do input está vazio e retornando uma mensagem para o usuario

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 'use strict';
+/*
 document.querySelector('.message').textContent = 'Correct Number! 🎉';
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
+*/
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number';
+  }
+});
